893827ee9702336541e9d347e98eaa25dee2e5cf
m852456951753 <m85245695175322@gmail.com>
2016-09-13 16:14:24 +0800

    0.0.0

---
+++ index.php
@@ -0,0 +1,175 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="font-awesome-4.6.3/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="js/flexslider/flexslider.css">
+    <title>Document</title>
+    <style>
+.flex-control-nav {
+  width:80%;
+  top:860px;
+}
+</style>
+</head>
+
+<body>
+    <div id="header">
+        <div class="container">
+            <div id="menuicon"><a class="db" href="#"><span class="Sronly">menuicon</span></a></div>
+            <h1 id="logo"><a class="db" href="index.php"><span class="Sronly">Cake House</span></a></h1>
+            <h2 class="Sronly">美味甜點新主張!綿密口感，甜在心頭超好吃!</h2>
+            <!-- 會員相關區塊 -->
+            <div id="membercontent">
+                <div id="membericon"><a href="" class="db"><span class="Sronly">會員登入</span></a></div>
+                <div id="carticon"><a href="" class="db"><span class="Sronly">購物車</span></a></div>
+            </div>
+            <!-- 會員相關區塊 -->
+            <nav id="primarynavigation">
+                <ul>
+                    <li><a href="#" class="db"><span >關於我們</span></a></li>
+                    <li><a href="#" class="db"><span >最新消息</span></a></li>
+                    <li><a href="#" class="db"><span >商品資訊</span></a></li>
+                    <li><a href="#" class="db"><span >聯絡我們</span></a></li>
+                </ul>
+        </div>
+    </div>
+    <div id="banner">
+        <div id="bannerleft"><img src="images/bg/banner_left_bg.png" alt=""></div>
+        <div id="bannercontent">
+          <ul class="slides">
+        <li>
+          <img src="uploads/banner/banner1.jpg" />
+        </li>
+        <li>
+          <img src="uploads/banner/banner2.jpg" />
+        </li>
+        <li>
+          <img src="uploads/banner/banner3.jpg" />
+        </li>
+        <li>
+          <img src="uploads/banner/banner4.jpg" />
+        </li>
+      </ul>
+        </div>
+        <div id="bannerright"><img src="images/bg/banner_right_bg.png" alt=""></div>
+    </div>
+    <div id="news">
+        <div class="container">
+            <h4>最新消息</h4>
+            <!-- 新聞區塊 -->
+            <div class="content">
+                <div class="wrap">
+                    <img src="uploads/news/news_01.jpg" alt="" class="newsimg">
+                    <p class="newsdate">dwdad</p>
+                    <p class="newstitle">wdawdwadaw</p>
+                </div>
+            </div>
+            <div class="content">
+                <div class="wrap">
+                    <img src="uploads/news/news_02.jpg" alt="" class="newsimg">
+                    <p class="newsdate">awdwd</p>
+                    <p class="newstitle">wdad</p>
+                </div>
+            </div>
+            <div class="content">
+                <div class="wrap">
+                    <img src="uploads/news/news_03.jpg" alt="" class="newsimg">
+                    <p class="newsdate">awdawd</p>
+                    <p class="newstitle">wdww</p>
+                </div>
+            </div>
+            <!-- 新聞區塊END -->
+        </div>
+    </div>
+    <div class="container">
+        <h4 class="Sronly">產品介紹</h4>
+        <div class="pccontent">
+            <div class="pcimgcontent">
+                <img src="uploads/product_category/product_category_01.jpg" alt=""></div>
+            <div class="pcdecription">手工巧克力</div>
+        </div>
+        <div class="pccontent">
+            <div class="pcimgcontent pcimgright">
+                <img src="uploads/product_category/product_category_02.jpg" alt="">
+            </div>
+            <div class="pcdecription">伴手禮盒</div>
+        </div>
+        <div class="pccontent">
+            <div class="pcimgcontent">
+                <img src="uploads/product_category/product_category_03.jpg" alt="">
+            </div>
+            <div class="pcdecription">彌月禮盒</div>
+        </div>
+    </div>
+    <div id="taste">
+        <div class="container">
+            <h4>好吃的秘訣</h4>
+            <!-- 秘訣區塊 -->
+            <div class="content">
+                <div class="wrap">
+                    <img src="images/taste/taste_01.jpg" alt="" class="newsimg">
+                    <div class="tiptag"></div>
+                    <p>嚴選新鮮、實在</p>
+                </div>
+            </div>
+            <!-- 秘訣區塊END -->
+            <!-- 秘訣區塊 -->
+            <div class="content">
+                <div class="wrap">
+                    <img src="images/taste/taste_02.jpg" alt="" class="newsimg">
+                    <div class="tiptag"></div>
+                    <p>嚴選新鮮、實在</p>
+                </div>
+            </div>
+            <!-- 秘訣區塊END -->
+            <!-- 秘訣區塊 -->
+            <div class="content">
+                <div class="wrap">
+                    <img src="images/taste/taste_03.jpg" alt="" class="newsimg">
+                    <div class="tiptag"></div>
+                    <p>嚴選新鮮、實在</p>
+                </div>
+            </div>
+            <!-- 秘訣區塊END -->
+        </div>
+    </div>
+    <div id="map">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3618.1149599903943!2d121.1625196503348!3d24.92815334860246!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x346824a20644729b%3A0xe4eeaaf4f13ab40!2zMzI25qGD5ZyS5biC5qWK5qKF5Y2A6auY542F6LevODEz5be3MjLlvIQxMeiZnw!5e0!3m2!1szh-TW!2stw!4v1472545166313"
+            width="100%" height="440" frameborder="0" style="border:0" allowfullscreen></iframe>
+    </div>
+    <!-- 頁尾 -->
+    <footer id="footer">
+        <div class="container">
+            <div id="contactcontent">
+                <div id="footerlogo"><a href="index.php">cakehouse</a></div>
+                <p>桃園市楊梅區高獅路813巷22弄11號</p>
+                <p>聯絡專線:0800-028-558</p>
+            </div>
+            <div id="societycontent">
+                <div id="facebookicon"><a href="" class="db"><i class="fa fa-facebook-square" aria-hidden="true"></i><span class="Sronly"></span></a></div>
+                <div id="googlepius"><a href="" class="db"><span class="Sronly">google+</span></a></div>
+            </div>
+        </div>
+        <div id="copyrights">
+            <p>Copyright © 2016 CakeHouse. All Rights Reserved.</p>
+        </div>
+    </footer>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+
+<script src="js/flexslider/jquery.flexslider.js"></script>
+<script>
+$(window).load(function(){
+      $('#BannerContent').flexslider({
+        animation: "slide",
+
+      });
+    });
+</script>
+</body>
+
+</html>
